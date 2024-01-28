85a54dac1571f875a1478d144a9074b8222ccd47
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-28 23:23:48 +0600

    Controller File Update

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -1,23 +1,19 @@
 exports.CreateStudent=(req,res)=>{
 
     res.end("I am Create Student")
-
 }
 
 exports.DeleteStudent=(req,res)=>{
 
     res.end("I am Delete Student")
-
 }
 
 exports.UpdateStudent=(req,res)=>{
 
     res.end("I am Update Student")
-
 }
 
 exports.ReadStudent=(req,res)=>{
 
     res.end("I am Read Student")
-
 }
