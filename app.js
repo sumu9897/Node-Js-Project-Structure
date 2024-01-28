43928e9f3941c5ec4d25a5f962d1778ff9da171c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-28 23:33:50 +0600

    hpp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const cors=require('cors')
 const helmet=require('helmet')
+const  hpp=require('hpp')
 const router= require('./src/routes/api')
 const  obj=new express()
 
@@ -8,6 +9,7 @@ const  obj=new express()
 obj.use('/',router)
 obj.use(cors)
 obj.use(helmet())
+obj.use(hpp)
 
 
 
