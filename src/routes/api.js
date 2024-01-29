23c61b84d10a98656032cd844d57fe8a142545cb
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-29 22:09:52 +0600

    Middleware file

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,13 @@
 const express=require('express');
 const StudentController =require("../controllers/StudentController")
+const AuthMiddleware= require("../middleware/AuthMiddleware")
 
 const router=express.Router();
 
 router.get("/read",StudentController.ReadStudent)
 
 
-router.post("/create",StudentController.CreateStudent)
+router.post("/create",AuthMiddleware,StudentController.CreateStudent)
 
 router.put("/update",StudentController.UpdateStudent)
 
