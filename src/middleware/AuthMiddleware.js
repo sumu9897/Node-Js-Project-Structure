1d315c4c849d7f8789e1ceb1ad58ca7e7f131820
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-29 22:16:00 +0600

    Middleware file

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -1,5 +1,8 @@
 module.exports=(req,res,next)=>{
 
-    console.log("I am AuthMiddleware")
-    next()
+    // console.log("I am AuthMiddleware")
+    // next()
+    // return res.end("Stop Here")
+
+    return next()
 }
